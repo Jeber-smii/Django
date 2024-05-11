a6e8e58a3339734b3fe9ce1bd48bea307c9e73a2
jeber-smii <jebersmii@gmail.com>
2024-05-11 17:37:07 +0100

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-mwd_t!(lauqvd+!sdl_#qj(u=tv#n@x(su45_sxxj-*c#w(7en
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["jebersmii.pythonanywhere.com"]
+ALLOWED_HOSTS = ["127.0.0.1","jebersmii.pythonanywhere.com"]
 
 
 # Application definition
